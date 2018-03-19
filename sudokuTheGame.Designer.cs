9e1ded59d2d7e9d7e12a60b9ab23f2a48d78b9a4
111lomaz111 <lomaz111@poczta.onet.pl>
2018-03-20 00:17:24 +0100

    Add project files.

---
+++ sudokuTheGame.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace SudokuTheGame
+{
+    partial class sudokuTheGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.Panel();
+            this.bttStartGame = new System.Windows.Forms.Button();
+            this.cbLevels = new System.Windows.Forms.ComboBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(220, 12);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(250, 250);
+            this.panel.TabIndex = 3;
+            this.panel.UseWaitCursor = true;
+            this.panel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Paint);
+            // 
+            // bttStartGame
+            // 
+            this.bttStartGame.Location = new System.Drawing.Point(139, 12);
+            this.bttStartGame.Name = "bttStartGame";
+            this.bttStartGame.Size = new System.Drawing.Size(75, 23);
+            this.bttStartGame.TabIndex = 4;
+            this.bttStartGame.Text = "Start!";
+            this.bttStartGame.UseVisualStyleBackColor = true;
+            this.bttStartGame.UseWaitCursor = true;
+            this.bttStartGame.Click += new System.EventHandler(this.bttStartGame_Click);
+            // 
+            // cbLevels
+            // 
+            this.cbLevels.FormattingEnabled = true;
+            this.cbLevels.Location = new System.Drawing.Point(12, 12);
+            this.cbLevels.Name = "cbLevels";
+            this.cbLevels.Size = new System.Drawing.Size(121, 21);
+            this.cbLevels.TabIndex = 5;
+            this.cbLevels.Text = "Wybierz poziom gry!";
+            this.cbLevels.UseWaitCursor = true;
+            this.cbLevels.SelectedIndexChanged += new System.EventHandler(this.cbLevels_SelectedIndexChanged);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(54, 109);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 6;
+            this.maskedTextBox1.UseWaitCursor = true;
+            // 
+            // sudokuTheGame
+            // 
+            this.AcceptButton = this.bttStartGame;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.cbLevels);
+            this.Controls.Add(this.bttStartGame);
+            this.Controls.Add(this.panel);
+            this.Name = "sudokuTheGame";
+            this.Text = "Sudoku the Game";
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.sudokuTheGame_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button bttStartGame;
+        private System.Windows.Forms.ComboBox cbLevels;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+    }
+}
+
---
+++ sudokuTheGame.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SudokuTheGame
+{
+    public partial class sudokuTheGame : Form
+    {
+        TextBox[,] textBoxes = new TextBox[9, 9];
+
+        int level;
+
+        Random randomNumber = new Random();
+
+        public sudokuTheGame()
+        {
+            InitializeComponent();
+            generateGrid();
+            guiTextes();
+        }
+
+        void guiTextes()
+        {
+            //add list to combo box for choose level
+            cbLevels.Items.Add("Łatwy");
+            cbLevels.Items.Add("Średni");
+            cbLevels.Items.Add("Trudny");
+        }
+
+        private void generateGrid()
+        {
+            int positionX = 0;
+            int positionY = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    textBoxes[i, j] = new TextBox();
+                    panel.Controls.Add(textBoxes[i, j]);
+                    textBoxes[i, j].Width = 20;
+                    textBoxes[i, j].Top = positionY;
+                    textBoxes[i, j].Left = positionX;
+                    textBoxes[i, j].TextAlign = HorizontalAlignment.Center;
+                    //textBoxes[i, j].Text =  i.ToString() + j.ToString(); //show id equal to id in array on GUI
+                    positionX = positionX + 26;
+                }
+                positionY = positionY + 26;
+                positionX = 0;
+            }
+        }
+
+        private void panel_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen p = new Pen(Color.Red, 4);
+            g.DrawLine(p, 75, 0, 75, 228);
+            g.DrawLine(p, 153, 0, 153, 228);
+            g.DrawLine(p, 0, 75, 228, 75);
+            g.DrawLine(p, 0, 153, 228, 153);
+        }
+
+        private void bttStartGame_Click(object sender, EventArgs e)
+        {
+            switch (level)
+            {
+                case 1:
+                    //MessageBox.Show("Easy");
+                    fillTextBoxes(70);
+                    break;
+                case 2:
+                    //MessageBox.Show("Medium");
+                    fillTextBoxes(50);
+                    break;
+                case 3:
+                    //MessageBox.Show("Hard");
+                    fillTextBoxes(10);
+                    break;
+                default:
+                    MessageBox.Show("Wybierz poziom trudności!");
+                    break;
+            }
+        }
+
+        private void cbLevels_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbLevels.SelectedIndex == 0)
+            {
+                level = 1;//easy level
+            }
+            else if (cbLevels.SelectedIndex == 1)
+            {
+                level = 2;//medium level
+            }
+            else if (cbLevels.SelectedIndex == 2)
+            {
+                level = 3;//hard level
+            }
+            else return;
+        }
+
+        private void fillTextBoxes(int amount)
+        {
+            int i, j;
+            for (int a = 0; a < amount; a++)
+            {
+                i = randomNumber.Next(0, 9);
+                j = randomNumber.Next(0, 9);
+                textBoxes[i, j].Text = randomNumber.Next(1, 9).ToString();
+                textBoxes[i, j].ReadOnly = true;
+            }
+        }
+
+        private void sudokuTheGame_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
